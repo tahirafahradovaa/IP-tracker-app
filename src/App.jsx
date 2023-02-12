ab80bb7c6e788226d3ed72a7c4a860ff84155ebd
tahirafahradovaa <tairafahrad@icloud.com>
2023-02-12 16:03:36 +0400

    simple app where you can search location by ip adresses

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Information from "./component/Information";
 import Map from "./component/Map";
+import arrow from "./design/arrow.svg";
 const App = () => {
   const [location, setLocation] = useState({ lat: 40.3909, lng: 49.8759 });
   const [ip, setIp] = useState("188.253.229.29");
@@ -30,12 +31,27 @@ const App = () => {
   return (
     <>
       <div className="inputContainer">
-        <input
-          onChange={(event) => setIp(event.target.value)}
-          type="text"
-          placeholder="enter your ip address"
-        />
-        <button onClick={handleClick}> </button>
+        <h1
+          style={{
+            color: "white",
+          }}
+        >
+          IP Address Tracker
+        </h1>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <input
+            onChange={(event) => setIp(event.target.value)}
+            type="text"
+            placeholder="enter your ip address"
+          />
+          <button onClick={handleClick}>
+            <img src={arrow} alt="" />{" "}
+          </button>
+        </div>
       </div>
       {data && <Information data={data} />}
       <Map location={location} />
